007e3c0549e6810156f573bd87c35fac4b7d8c1c
Matthew Brotherton <mebrotherton64@gmail.com>
2024-12-02 11:09:42 -0500

    moved the DB stuff into the correct folder under java

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,12 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+insert into breed(breed_name)
+values  ('samoyed'),
+		('husky'),
+		('beagle'),
+		('french bulldog');
+
+
+
 COMMIT TRANSACTION;

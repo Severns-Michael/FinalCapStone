c02c280151bcaebcb5658ea448c8513879cfff01
Michael Severns <severns0319@gmail.com>
2024-12-02 14:08:56 -0500

    11/2/24 removed trait C U D

--- java/src/main/java/com/techelevator/dao/JDBCTraitDao.java
+++ java/src/main/java/com/techelevator/dao/JDBCTraitDao.java
@@ -47,53 +47,6 @@ public class JDBCTraitDao implements TraitDao{
         return trait;
     }
 
-    @Override
-    public Trait createTrait(Trait trait) throws DaoException {
-        int traitsId;
-        String sql = "insert into Traits (trait_name) values (?) RETURNING trait_id)"; //edit
-        try {
-            traitsId = jdbcTemplate.queryForObject(sql, int.class, trait.getTraitName());
-            trait = getTraitById(traitsId);
-        }
-        catch (CannotGetJdbcConnectionException e) {
-            throw new DaoException("Unable to connect to server or database", e);
-        } catch (DataIntegrityViolationException e) {
-            throw new DaoException("Data integrity violation", e);
-        }
-        return trait;
-    }
-
-    @Override
-    public Trait updateTrait(Trait trait) throws DaoException {
-        String sql = "update trait set trait_name=? where trait_id = ?"; // edit
-        try {
-            int rows = jdbcTemplate.update(sql, trait.getTraitName());
-            if (rows != 1) {
-                throw new DaoException("Unable to update traits");
-            }
-            trait = getTraitById(trait.getTraitId());
-        } catch (CannotGetJdbcConnectionException e) {
-            throw new DaoException("Unable to connect to server or database", e);
-        } catch (DataIntegrityViolationException e) {
-            throw new DaoException("Data integrity violation", e);
-        }
-        return trait;
-    }
-
-    @Override
-    public void deleteTrait(int traitsId) throws DaoException {
-        String sql = "delete from Traits where trait_id = ?"; //edit
-        String sql2 = "delete from Breed_trait where trait_id = ?";
-        try {
-            jdbcTemplate.update(sql, traitsId);
-            jdbcTemplate.update(sql2, traitsId);
-        } catch (CannotGetJdbcConnectionException e) {
-            throw new DaoException("Unable to connect to server or database", e);
-        } catch (DataIntegrityViolationException e) {
-            throw new DaoException("Data integrity violation", e);
-        }
-
-    }
 
     // MApRow
     public Trait mapRowToTrait(SqlRowSet rs) {
--- java/src/main/java/com/techelevator/dao/TraitDao.java
+++ java/src/main/java/com/techelevator/dao/TraitDao.java
@@ -6,7 +6,5 @@ import java.util.List;
 public interface TraitDao {
     List<Trait> listAllTraits() throws DaoException;
     Trait getTraitById(int id) throws DaoException;
-    Trait createTrait(Trait trait) throws DaoException;
-    Trait updateTrait(Trait trait) throws DaoException;
-    void deleteTrait(int traitsId) throws DaoException;
+
 }

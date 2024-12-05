01b86e0c66da6ab2299301c2f4962b162bc847a3
Brandon Shick <brandon.shick97@gmail.com>
2024-12-05 12:46:33 -0500

    pushing progress on dogcard

---
+++ vue/src/components/DogCard.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+        <img v-bind:src="this.currentDog.img"/>
+        <h2>{{ this.currentDog.dogName }}</h2>
+        <ul>
+            <li v-for="trait in this.currentDogBreed.traits" v-bind:key="trait.traitId"> {{ trait.traitName }} </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import DogService from '../services/DogService';
+import BreedService from '../services/BreedService';
+
+export default {
+    props: {
+        dog: {}
+    },
+    data() {
+        return {
+            currentDog: this.dog,
+            currentDogBreed: {},
+        }
+    },
+    beforeMount() {
+        this.getDogBreed();
+       
+    },
+    methods: {
+        getDogBreed() {
+            BreedService.getBreedById(this.currentDog.breedId).then(response => {
+                this.currentDogBreed = response.data;
+                this.getDogPic();
+            });
+        },
+        getDogPic() {
+            if (this.currentDog.img === null) {
+                if (!this.currentDogBreed.subBreed === null) {
+                    DogService.getSubBreedPic(this.currentDogBreed.breedName, this.currentDogBreed.subBreed).then(response => {
+                        this.currentDog.img = response.data.message
+                    });
+                } else {
+                    DogService.getBreedPic(this.currentDogBreed.breedName).then(response => {
+                        this.currentDog.img = response.data.message
+                    });
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        border: 1px solid black;
+    }
+</style>
---
+++ vue/src/views/UserDashboardView.vue
@@ -0,0 +1,37 @@
+<template>
+    <router-link v-bind:to="{name: 'setcriteria'}"> Set Preferences </router-link>
+
+    <ul class="selectedDogList">
+        <li v-for="dog in this.yesDogs" v-bind:key="dog.dogId">
+            <dog-card v-bind:dog="dog"></dog-card>
+        </li>
+    </ul>
+</template>
+
+<script>
+import DogCard from '../components/DogCard.vue';
+import DogService from '../services/DogService';
+
+export default {
+    components: {
+        DogCard
+    },
+    data() {
+        return {
+            yesDogs: [{dogId: 1, dogName: 'Rufus', breedId: 11, age: 10, size: 2, img: null}]
+        }
+    },
+    created() {
+        DogService.getDogsYes().then(response => {
+            this.yesDogs = response.data;
+        });
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+}
+
+</style>
